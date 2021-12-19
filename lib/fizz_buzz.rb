e0c09e16607550bd0f8576dbe96bf807cbac196e
syarikim <omojitex789@gmail.com>
2021-12-19 11:15:00 +0900

    fizzbuzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,11 @@
 def fizz_buzz(number)
-  # 実装してください
+  if number % 15 == 0
+    p 'FizzBuzz'
+  elsif number % 3 == 0
+    p 'Fizz'
+  elsif number % 5 == 0
+    p 'Buzz'
+  else
+    p number
+  end
 end
